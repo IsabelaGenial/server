79c15ac47b993875d16c6e569319ebe44ac9e28a
Isabela Genial <isabelamgenial@gmail.com>
2025-07-01 17:55:20 -0300

    '

--- mini_server.c
+++ mini_server.c
@@ -1,171 +1,191 @@
 #include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
-#include <sys/socket.h>
-#include <sys/select.h>
-#include <netinet/in.h>
+#include <netinet/ip.h>
 
-int server_fd,max_fd,next_id = 0;
-int client_id[60000];
-char *client_msg[60000];
-fd_set active_fds,read_fds;
-char send_buffer[1024 * 42];
-char recv_buffer[1024 * 42];
+int		count = 0, max_fd = 0;
+int		ids[65536];
+char	*msgs[65536];
 
-void fatal_error(){
-    write(2, "Fatal error\n", 12);
-    close(server_fd);
-    exit(1);
+fd_set	rfds, wfds, afds;
+char	buf_read[1001], buf_write[42];
+
+
+// ========================= COPIAR FUNCOES ABAIXO DO MAIN.C QUE VEM COM O SUBJECT  =========================
+
+int extract_message(char **buf, char **msg)
+{
+	char	*newbuf;
+	int	i;
+
+	*msg = 0;
+	if (*buf == 0)
+		return (0);
+	i = 0;
+	while ((*buf)[i])
+	{
+		if ((*buf)[i] == '\n')
+		{
+			newbuf = calloc(1, sizeof(*newbuf) * (strlen(*buf + i + 1) + 1));
+			if (newbuf == 0)
+				return (-1);
+			strcpy(newbuf, *buf + i + 1);
+			*msg = *buf;
+			(*msg)[i + 1] = 0;
+			*buf = newbuf;
+			return (1);
+		}
+		i++;
+	}
+	return (0);
 }
 
-void send_to_all(int sender_fd){
-    for(int fd = 0; fd <= max_fd; fd++){
-        if(FD_ISSET(fd, &active_fds) && fd != sender_fd && fd != server_fd){
-            if(send(fd, send_buffer, strlen(send_buffer), 0) < 0){
-                //print_aqui_para_debug
-            }
-        }
-    }
+char *str_join(char *buf, char *add)
+{
+	char	*newbuf;
+	int		len;
+
+	if (buf == 0)
+		len = 0;
+	else
+		len = strlen(buf);
+	newbuf = malloc(sizeof(*newbuf) * (len + strlen(add) + 1));
+	if (newbuf == 0)
+		return (0);
+	newbuf[0] = 0;
+	if (buf != 0)
+		strcat(newbuf, buf);
+	free(buf);
+	strcat(newbuf, add);
+	return (newbuf);
 }
 
-void accept_new_client(){
-    struct sockaddr_in client_addr;
-    socklen_t len = sizeof(client_addr);
-    int client_fd;
-    
-    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &len);
-    if(client_fd < 0){
-        return;
-    }
+// ========================= FIM DO COPIAR E COLAR =========================
 
-    if(client_fd > max_fd)
-        max_fd = client_fd;
 
-    client_id[client_fd] = next_id++;
-    client_msg[client_fd] = NULL;
-
-    FD_SET(client_fd, &active_fds);
-
-    sprintf(send_buffer, "server: client %d just arrived\n", client_id[client_fd]);
-    send_to_all(client_fd);
-
+void	fatal_error()
+{
+	write(2, "Fatal error\n", 12);
+	exit(1);
 }
 
-void process(int fd){
-    int bytes_recv = recv(fd, recv_buffer, sizeof(recv_buffer) - 1, 0);
-
-    if(bytes_recv <= 0){
-        sprintf(send_buffer,  "server: client %d just left\n", client_id[fd]);
-        send_to_all(fd);
-
-        if(client_msg[fd])
-        {
-            free(client_msg[fd]);
-            client_msg[fd] = NULL;
-        }
-        close(fd);
-        FD_CLR(fd, &active_fds);
-        return;
-    }
-
-    recv_buffer[bytes_recv] = '\0';
-
-    if(client_msg[fd] == NULL){
-        client_msg[fd] = malloc (strlen(recv_buffer) + 1);
-        if(!client_msg[fd])
-            fatal_error();
-        strcpy(client_msg[fd], recv_buffer);
-    }else{
-        char *temp = realloc(client_msg[fd], strlen(client_msg[fd]) + strlen(recv_buffer) + 1);
-        if(!temp)
-            fatal_error();
-        client_msg[fd] = temp;
-        strcat(client_msg[fd], recv_buffer);
-
-    }
-
-    char *line_start = client_msg[fd];
-    char *newline_pos;
-    while((newline_pos = strstr(line_start, "\n"))){
-        *newline_pos = '\0';
-
-        sprintf(send_buffer, "client %d: %s\n", client_id[fd], line_start);
-        send_to_all(fd);
-
-        line_start = newline_pos + 1;
-    }
-
-    if(*line_start){
-        char *remaining_msg = malloc(strlen(line_start) + 1);
-        if(remaining_msg == NULL) fatal_error();
-        free(client_msg[fd]);
-        client_msg[fd] = remaining_msg;
-        strcpy(client_msg[fd], line_start);     
-    }else{
-
-        free(client_msg[fd]);
-        client_msg[fd] = NULL;
-    }
-
-
-
+void	notify_other(int author, char *str)
+{
+	for (int fd = 0; fd <= max_fd; fd++)
+	{
+		if (FD_ISSET(fd, &wfds) && fd != author)
+			send(fd, str, strlen(str), 0);
+	}
 }
 
-int main(int argc, char **argv) {
-    if(argc != 2)
-    {
-        write(2, "Wrong number of arguments\n", 26);
-        exit(1);
-    }
+void	register_client(int fd)
+{
+	max_fd = fd > max_fd ? fd : max_fd;
+	ids[fd] = count++;
+	msgs[fd] = NULL;
+	FD_SET(fd, &afds);
+	sprintf(buf_write, "server: client %d just arrived\n", ids[fd]);
+	notify_other(fd, buf_write);
+}
 
-	
-    struct sockaddr_in servaddr; 
-	// socket create and verification 
-	server_fd = socket(AF_INET, SOCK_STREAM, 0); 
-	if (server_fd == -1) { 
-        fatal_error(); 
-	} 
+void	remove_client(int fd)
+{
+	sprintf(buf_write, "server: client %d just left\n", ids[fd]);
+	notify_other(fd, buf_write);
+	free(msgs[fd]);
+	FD_CLR(fd, &afds);
+	close(fd);
+}
 
-	FD_ZERO(&active_fds);
-	bzero(&servaddr, sizeof(servaddr));
-    bzero(client_id, sizeof(servaddr));
-    bzero(client_msg, sizeof(client_msg));
+void	send_msg(int fd)
+{
+	char *msg;
 
-    FD_SET(server_fd, &active_fds);
-    max_fd = server_fd;
+	while (extract_message(&(msgs[fd]), &msg))
+	{
+		sprintf(buf_write, "client %d: ", ids[fd]);
+		notify_other(fd, buf_write);
+		notify_other(fd, msg);
+		free(msg);
+	}
+}
 
-	// assign IP, PORT 
-	servaddr.sin_family = AF_INET; 
-	servaddr.sin_addr.s_addr = htonl(2130706433); //127.0.0.1
-	servaddr.sin_port = htons(atoi(argv[1])); 
-  
-	// Binding newly created socket to given IP and verification 
-	if ((bind(server_fd, (const struct sockaddr *)&servaddr, sizeof(servaddr))) < 0) { 
-		fatal_error(); 
-	} 
-	
-	if (listen(server_fd, 128) < 0) {
+int		create_socket()
+{
+	max_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if (max_fd < 0)
 		fatal_error();
+	FD_SET(max_fd, &afds);
+	return max_fd;
+}
+
+int		main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		write(2, "Wrong number of arguments\n", 26);
+		exit(1);
 	}
 
-    while(369){
-        read_fds = active_fds;
-        if(select(max_fd + 1, &read_fds, NULL, NULL, 0) < 0){
-            continue;
-        }
+	FD_ZERO(&afds);
+	int sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0)
+		fatal_error();
+	FD_SET(sockfd, &afds);
+    max_fd = sockfd;
 
-        for(int fd = 0; fd <= max_fd; fd++){
-            if(FD_ISSET(fd, &read_fds)){
-                if(fd == server_fd){
-                    accept_new_client();
-                    break;
-                }else{
-                    process(fd);
-                }
-            }
-        }
-    }
+// ========================= ADAPTAR O TRECHO ABAIXO DO MAIN.C QUE VEM COM O SUBJECT  =========================
 
-    return 0;
+	struct sockaddr_in servaddr;
+	bzero(&servaddr, sizeof(servaddr));
+
+	servaddr.sin_family = AF_INET;
+	servaddr.sin_addr.s_addr = htonl(2130706433);
+	servaddr.sin_port = htons(atoi(av[1])); // replace 8080
+
+	if (bind(sockfd, (const struct sockaddr *)&servaddr, sizeof(servaddr)))
+		fatal_error();
+	if (listen(sockfd, SOMAXCONN)) // the main uses 10, SOMAXCONN is 180 on my machine
+		fatal_error();
+
+
+	// ========================= ADAPTAR FIM  =========================
+
+	while (1)
+	{
+		rfds = wfds = afds;
+
+		if (select(max_fd + 1, &rfds, &wfds, NULL, NULL) < 0)
+			fatal_error();
+
+		for (int fd = 0; fd <= max_fd; fd++)
+		{
+			if (!FD_ISSET(fd, &rfds))
+				continue;
+
+			if (fd == sockfd)
+			{
+				int client_fd = accept(sockfd, NULL, NULL);
+				if (client_fd >= 0)
+				{
+					register_client(client_fd);
+					break;
+				}
+			}
+			else
+			{
+				int read_bytes = recv(fd, buf_read, 1000, 0);
+				if (read_bytes <= 0)
+				{
+					remove_client(fd);
+					break ;
+				}
+				buf_read[read_bytes] = '\0';
+				msgs[fd] = str_join(msgs[fd], buf_read);
+				send_msg(fd);
+			}
+		}
+	}
+	return 0;
 }
